328b8eef544a24f4472b0b3e4dfada2a9801f73b
DreamEnderKing <www20040718@163.com>
2021-08-16 15:44:31 +0800

    Scalar updated.

---
+++ SimBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulationBase
+{
+    struct Scalar
+    {
+        public Scalar(double d) => val = d;
+        private double val;
+        public static implicit operator double(Scalar s) => s.val;
+        public static implicit operator float(Scalar s) => (float)s.val;
+        public static implicit operator Scalar(double d) => new Scalar(d);
+    }
+}
